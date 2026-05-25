b0a58bb3aa67ad4ba0606dbb07ef5b133eaef666
Felipe Rosa <felipe.sgrosa@gmail.com>
2026-05-25 01:19:06 -0300

    list toplevel keys in torrent file in main

--- bin/main.ml
+++ bin/main.ml
@@ -2,5 +2,8 @@ let () =
   let s = In_channel.with_open_bin Sys.argv.(1) In_channel.input_all in
   match Bencode.parse_value (Bencode.Parsing.ByteSource.of_string s) with
   | None -> print_endline "parsing failed"
-  | Some _ -> print_endline "parsing ok"
+  | Some (v, _) ->
+    v
+    |> Bencode.Value.get_dict
+    |> Bencode.Value.StringMap.iter (fun k _ -> print_endline k)
 ;;
